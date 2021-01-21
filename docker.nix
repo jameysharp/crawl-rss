51fab3d7f1c164afdb56a9ce2f225f0a9d242935
Jamey Sharp <jamey@minilop.net>
2021-01-20 22:51:58 -0800

    Fix container name resolution in Docker deployment
    By adding a minimal /etc/nsswitch.conf to the image.

--- docker.nix
+++ docker.nix
@@ -8,10 +8,14 @@ in pkgs.dockerTools.streamLayeredImage {
   name = "crawl-rss";
   contents = [
     (app.dependencyEnv.override {
-      # Symlink the app itself into a common location. Since the resulting
-      # environment is part of the "contents" attribute, this symlink will be
-      # available at the root of the generated Docker image.
       postBuild = ''
+        # glibc doesn't have defaults for NSS any more I guess?
+        mkdir -p $out/etc
+        echo "hosts: files dns" > $out/etc/nsswitch.conf
+
+        # Symlink the app itself into a common location. Since the resulting
+        # environment is part of the "contents" attribute, this symlink will be
+        # available at the root of the generated Docker image.
         ln -s ${app}/${app.python.sitePackages} $out/app
       '';
     })
